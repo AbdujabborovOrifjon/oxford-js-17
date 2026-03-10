95d448f2319f987a650b6129355047044b794811
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-03-10 16:55:34 +0500

    change

--- 47-dars-class-style/index.js
+++ 47-dars-class-style/index.js
@@ -1,26 +1,64 @@
-let cont = document.querySelector(".cont")
+// let cont = document.querySelector(".cont")
 
-cont.style.cssText = `
+// cont.style.cssText = `
+// width: 200px;
+// height: 200px;
+// border: 2px solid blue;
+// `
+
+// cont.classList.add("active");
+
+
+// let btn = document.querySelector(".btn")
+// btn.addEventListener("click", function() {
+//   cont.classList.toggle("active")
+// })
+
+// let btn2 = document.querySelector(".btn2")
+
+// btn2.addEventListener("click", function () {
+// cont.classList.replace("active", "active2")
+    
+// })
+
+// console.log("hello world");
+
+// alert("hello world")
+
+
+
+
+let titles = document.querySelector(".titles")
+
+titles.className = "title"
+let title = document.querySelector(".title")
+
+if (title) {
+title.style.cssText = `
+color:red;
+`
+}else{
+    alert("malumot topilmadi")
+}
+
+
+
+let box = document.querySelector(".box")
+box.style.cssText = `
 width: 200px;
 height: 200px;
-border: 2px solid blue;
+border:2px solid blue;
+margin: 50px auto;
+
 `
 
-cont.classList.add("active");
+
+// box.classList.add("active")
+box.classList.remove("box")
 
 
 let btn = document.querySelector(".btn")
-btn.addEventListener("click", function() {
-  cont.classList.toggle("active")
+btn.addEventListener("click", ()=>{
+    box.classList.toggle("active")
 })
 
-let btn2 = document.querySelector(".btn2")
-
-btn2.addEventListener("click", function () {
-cont.classList.replace("active", "active2")
-    
-})
-
-console.log("hello world");
-
-alert("hello world")
---
+++ 48-dars-events/index.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector(".btn")
+let btn2 = document.querySelector(".btn2")
+
+btn.addEventListener("click", function () {
+    console.log("click ishladi");
+    
+})
+
+btn2.addEventListener("dblclick", function () {
+    console.log("abort  ishladi");
+    
+})
+
